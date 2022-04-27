a6a4128a8317a4e2a64ea23e06d03f706af086a5
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-04-27 16:38:19 +0530

    improving setup 🔥

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
